2cd8c993924a5312d6ff0359429435e78fd70974
Maxence POUTORD <maxence.poutord@gmail.com>
2015-09-21 14:25:35 +0200

    add php quality tools

--- provision/pimpMyBox.sh
+++ provision/pimpMyBox.sh
@@ -11,7 +11,10 @@ sudo apt-get upgrade -y
 sudo apt-get install php5-xdebug -y
 sudo service apache2 restart
 
-# Jekyll
+
+##########
+# Jekyll #
+##########
 sudo gem install bundler
 sudo gem install jekyll
 sudo bundle install
@@ -19,16 +22,43 @@ sudo bundle install
 alias cls="clear"
 alias cdp="cd /var/www/public"
 
-# Add Composer to PATH.
-export PATH="~/.composer/vendor/bin:$PATH"
 
-
-# Mail Catcher
+################
+# Mail Catcher #
+################
 gem install mailcatcher
 mailcatcher --http-ip=0.0.0.0 -f
 #in /etc/php5/apache2/php.ini add this line sendmail_path = /usr/bin/env catchmail
 
 # If don't work, install Mailcatcher Dependencies (sqlite, ruby)
 # sudo apt-get install -y libsqlite3-dev ruby1.9.1-dev
+
+
+##################
+# COMPOSER TOOLS #
+##################
+# Add Composer to PATH.
+export PATH="~/.composer/vendor/bin:$PATH"
+
+# PHP copy/past detector + dead code detector
+composer global require "sebastian/phpcpd=*"
+composer global require 'sebastian/phpdcd=*'
+
+# PDEPEND
+composer global require "pdepend/pdepend"
+
+# PHPMD
+# if fail, copy/past this 3 lines : https://getcomposer.org/doc/articles/troubleshooting.md#proc-open-fork-failed-errors
+sudo composer global require "phpmd/phpmd"
+# example : phpmd path/to/file.php text codesize,unusedcode,naming
+
+# PHPCS
+composer global require "squizlabs/php_codesniffer=*"
+
+# phpmetrics
+phpmetrics --report-html=myreport.html cvq/evo2015/application/
+
+# phploc
+composer global require 'phploc/phploc=*'
 
 echo "Installation completed !"
